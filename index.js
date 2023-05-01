43357adc9d1dc5901549ba221080f667cfa5c555
Lingfan Gao <lingfangao@hotmail.com>
2023-05-01 13:42:54 +0200

    doc: Adds jsdoc to echo
    Documents the echo function and its paramters and options.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/**
+ * Echo user provided value
+ * 
+ * @param {string} value - The value to echo
+ * @param {object} options
+ * @param {number} [options.count=5] - How many times to echo
+ * @param {string} [options.prefix=echo:] - Prefix logged before echoing
+ */
 function echo(value, options) {
   const { count = 5, prefix = 'echo:'} = options
 
